53e8275ed8cac789632b43b2f1cf79bce6d32b14
dimak <github@dimak.dev>
2023-04-14 10:23:11 +0200

    Implemented the finding function for Auto Moving Reducer

--- src/utils/findPositions.test.ts
+++ src/utils/findPositions.test.ts
@@ -2,7 +2,7 @@ import {IGameBoard} from "Types/IGameBoard";
 import {ICard} from "Types/ICard";
 import {ECardSuit} from "Types/ECardSuit";
 import {ECardValue} from "Types/ECardValue";
-import {findCurrentPosition} from "Utils/findPositions";
+import {findCurrentPosition, findTargetPositions} from "Utils/findPositions";
 import {EGameBoardPart} from "Types/EGameBoardPart";
 
 describe('Find positions of selected card', function () {
@@ -140,5 +140,148 @@ describe('Find positions of selected card', function () {
 
     });
 
+    describe('Find target positions', () => {
+        describe('KING can occupy all empty piles on tableau', () => {
+            let targetPositions: any[];
 
+            beforeEach(() => {
+                targetPositions = [];
+            });
+
+            afterEach(() => {
+                const targetPiles = targetPositions
+                    .map(target => target.position === EGameBoardPart.TABLEAU && target.pileId);
+
+                expect(targetPiles).toHaveLength(7);
+                expect(targetPiles).toContain('test-pile-1');
+                expect(targetPiles).toContain('test-pile-2');
+                expect(targetPiles).toContain('test-pile-3');
+                expect(targetPiles).toContain('test-pile-4');
+                expect(targetPiles).toContain('test-pile-5');
+                expect(targetPiles).toContain('test-pile-6');
+                expect(targetPiles).toContain('test-pile-7');
+            });
+
+            test('DIAMOND KING', () => {
+                targetPositions = findTargetPositions(state, {suit: ECardSuit.DIAMOND, value: ECardValue.KING});
+            });
+
+            test('HEART KING', () => {
+                targetPositions = findTargetPositions(state, {suit: ECardSuit.HEART, value: ECardValue.KING});
+            });
+
+            test('SPADE KING', () => {
+                targetPositions = findTargetPositions(state, {suit: ECardSuit.SPADE, value: ECardValue.KING});
+            });
+
+            test('CLUB KING', () => {
+                targetPositions = findTargetPositions(state, {suit: ECardSuit.CLUB, value: ECardValue.KING});
+            });
+        });
+
+        test('KING can\'t occupy already occupied pile by other card', () => {
+            const kingCard: ICard = {suit: ECardSuit.SPADE, value: ECardValue.KING};
+            state.tableau[2].cards.push({isOpen: true, card: {suit: ECardSuit.CLUB, value: ECardValue.FIVE}});
+
+            const piles = findTargetPositions(state, kingCard)
+                .map(target => target.position === EGameBoardPart.TABLEAU && target.pileId);
+
+            expect(piles).not.toContain('test-foundation-3');
+        });
+
+        test('KING can occupy only foundation with same suit and the last card in foundation is QUEEN', () => {
+            const kingCard: ICard = {suit: ECardSuit.HEART, value: ECardValue.KING};
+
+            state.foundations[1].suit = ECardSuit.HEART;
+            state.foundations[1].cards.push({suit: ECardSuit.HEART, value: ECardValue.QUEEN});
+
+            const foundations = findTargetPositions(state, kingCard)
+                .filter(({position}) => position === EGameBoardPart.FOUNDATIONS)
+                .map(target => target.position === EGameBoardPart.FOUNDATIONS && target.foundationId); // ts can`t cast types after array.filter
+
+            expect(foundations).toHaveLength(1);
+            expect(foundations).toContain('test-foundation-2');
+        });
+
+        test('KING can\'t occupy foundation if all foundation occupied by other cards (= not QUEEN)', () => {
+            const kingCard: ICard = {suit: ECardSuit.HEART, value: ECardValue.KING};
+
+            state.foundations[0].suit = ECardSuit.HEART;
+            state.foundations[0].cards.push({suit: ECardSuit.HEART, value: ECardValue.SIX});
+            state.foundations[1].suit = ECardSuit.CLUB;
+            state.foundations[1].cards.push({suit: ECardSuit.CLUB, value: ECardValue.ACE});
+            state.foundations[2].suit = ECardSuit.SPADE;
+            state.foundations[2].cards.push({suit: ECardSuit.SPADE, value: ECardValue.JACK});
+            state.foundations[3].suit = ECardSuit.DIAMOND;
+            state.foundations[3].cards.push({suit: ECardSuit.DIAMOND, value: ECardValue.EIGHT});
+
+            const foundations = findTargetPositions(state, kingCard)
+                .filter(({position}) => position === EGameBoardPart.FOUNDATIONS)
+                .map(target => target.position === EGameBoardPart.FOUNDATIONS && target.foundationId);
+
+            expect(foundations).toHaveLength(0);
+        });
+
+        test('THREE SPADE can occupy only foundation what contains TWO SPADE', () => {
+            const card: ICard = {suit: ECardSuit.SPADE, value: ECardValue.THREE};
+            state.foundations[1].suit = ECardSuit.DIAMOND;
+            state.foundations[1].cards.push({suit: ECardSuit.DIAMOND, value: ECardValue.TWO});
+            state.foundations[2].suit = ECardSuit.SPADE;
+            state.foundations[2].cards.push({suit: ECardSuit.SPADE, value: ECardValue.TWO});
+
+            const targets = findTargetPositions(state, card);
+
+            expect(targets).toHaveLength(1);
+            expect(targets[0]).toHaveProperty('position', EGameBoardPart.FOUNDATIONS);
+            expect(targets[0]).toHaveProperty('foundationId', 'test-foundation-3');
+        });
+
+        test('ACE can occupy all empty foundations', () => {
+            const card: ICard = {suit: ECardSuit.SPADE, value: ECardValue.ACE};
+
+            const foundationsIds = findTargetPositions(state, card)
+                .filter(target => target.position == EGameBoardPart.FOUNDATIONS)
+                .map(target => target.position == EGameBoardPart.FOUNDATIONS && target.foundationId);
+
+            expect(foundationsIds).toHaveLength(4);
+            expect(foundationsIds).toContain('test-foundation-1');
+            expect(foundationsIds).toContain('test-foundation-2');
+            expect(foundationsIds).toContain('test-foundation-3');
+            expect(foundationsIds).toContain('test-foundation-4');
+        });
+
+        test('Black ACE can occupy two piles with Red ONE', () => {
+            const card: ICard = {suit: ECardSuit.SPADE, value: ECardValue.ACE};
+
+            state.tableau[2].cards.push({isOpen: true, card: {suit: ECardSuit.DIAMOND, value: ECardValue.ONE}});
+            state.tableau[3].cards.push({isOpen: true, card: {suit: ECardSuit.SPADE, value: ECardValue.ONE}});
+            state.tableau[4].cards.push({isOpen: true, card: {suit: ECardSuit.HEART, value: ECardValue.ONE}});
+            state.tableau[5].cards.push({isOpen: true, card: {suit: ECardSuit.CLUB, value: ECardValue.ONE}});
+
+            const pilesIds = findTargetPositions(state, card)
+                .filter(target => target.position == EGameBoardPart.TABLEAU)
+                .map(target => target.position == EGameBoardPart.TABLEAU && target.pileId);
+
+            expect(pilesIds).toHaveLength(2);
+            expect(pilesIds).toContain('test-pile-3');
+            expect(pilesIds).toContain('test-pile-5');
+        });
+
+        test('Red SEVEN can occupy two piles with Black EIGHT', () => {
+            const card: ICard = {suit: ECardSuit.DIAMOND, value: ECardValue.SEVEN};
+
+            state.tableau[2].cards.push({isOpen: true, card: {suit: ECardSuit.DIAMOND, value: ECardValue.EIGHT}});
+            state.tableau[3].cards.push({isOpen: true, card: {suit: ECardSuit.SPADE, value: ECardValue.EIGHT}});
+            state.tableau[4].cards.push({isOpen: true, card: {suit: ECardSuit.HEART, value: ECardValue.EIGHT}});
+            state.tableau[5].cards.push({isOpen: true, card: {suit: ECardSuit.CLUB, value: ECardValue.EIGHT}});
+
+            const pilesIds = findTargetPositions(state, card)
+                .filter(target => target.position == EGameBoardPart.TABLEAU)
+                .map(target => target.position == EGameBoardPart.TABLEAU && target.pileId);
+
+            expect(pilesIds).toHaveLength(2);
+            expect(pilesIds).toContain('test-pile-4');
+            expect(pilesIds).toContain('test-pile-6');
+        });
+    });
 });
--- src/utils/findPositions.ts
+++ src/utils/findPositions.ts
@@ -1,12 +1,15 @@
-import {IGameBoard} from "Types/IGameBoard";
+import {IFoundation, IGameBoard, IPile} from "Types/IGameBoard";
 import {ICard} from "Types/ICard";
 import {EGameBoardPart} from "Types/EGameBoardPart";
+import {ECardValue} from "Types/ECardValue";
+import {nextByPriority, prevByPriority} from "Utils/prevOnNextCardValue";
+import {cardSuiteColor} from "Utils/cardSuiteColor";
 
 /**
  * Base type of position result.
  */
 type TBaseGameBoardPositionResult = {
-    position: EGameBoardPart,
+    // position: EGameBoardPart,
 };
 
 /**
@@ -36,6 +39,12 @@ type TGameBoardPositionResult = TFoundationGameBoardPositionResult
     | TTalonGameBoardPositionResult
     | TTableauGameBoardPositionResult;
 
+/**
+ * Find current position of selected card.
+ *
+ * @param {IGameBoard} state State.
+ * @param {ICard} card Selected card.
+ */
 export function findCurrentPosition(state: IGameBoard, card: ICard): TGameBoardPositionResult {
     const lastCardInTalon = !!state.talon.length && state.talon[state.talon.length - 1];
     if (lastCardInTalon && card.value == lastCardInTalon.value && card.suit == lastCardInTalon.suit) {
@@ -59,7 +68,12 @@ export function findCurrentPosition(state: IGameBoard, card: ICard): TGameBoardP
             pileId: id,
             cards: cards.filter(({isOpen}) => isOpen)
         }))
-        .find(({cards}) => cards.length && cards.find(({card: {suit, value}}) => card.value == value && suit == card.suit));
+        .find(({cards}) => cards.length && cards.find(({
+                                                           card: {
+                                                               suit,
+                                                               value
+                                                           }
+                                                       }) => card.value == value && suit == card.suit));
 
     if (cardInTableau) {
         return {
@@ -69,4 +83,68 @@ export function findCurrentPosition(state: IGameBoard, card: ICard): TGameBoardP
     }
 
     throw new Error('Interaction with this card is restricted');
+}
+
+/**
+ * Find all possible targets for selected cards.
+ *
+ * @param {IGameBoard} state State.
+ * @param {IGameBoard} card Selected card.
+ */
+export function findTargetPositions(state: IGameBoard, card: ICard): Array<TGameBoardPositionResult> {
+    const targetPositions: Array<TGameBoardPositionResult> = [];
+
+    // check in foundations
+    if (state.foundations.length) {
+        // calculate previous value by priority
+        const previousValueByPriority = prevByPriority(card.value);
+
+        // filter predicate for foundations
+        const filterPredicate = (foundation: IFoundation): boolean => {
+            if (card.value === ECardValue.ACE) {
+                if (!foundation.suit) return true;
+                if (!foundation.cards.length) return true;
+            }
+
+            if (foundation.suit == card.suit && foundation.cards.length) {
+                const lastCardInFoundation = foundation.cards[foundation.cards.length - 1];
+
+                if (lastCardInFoundation.value == previousValueByPriority) return true;
+            }
+
+            return false;
+        };
+
+        state.foundations
+            .filter(filterPredicate)
+            .forEach(foundation => targetPositions.push({
+                position: EGameBoardPart.FOUNDATIONS,
+                foundationId: foundation.id
+            }));
+    }
+
+    // check in tableau
+    if (state.tableau.length) {
+        // find all empty piles on tableau for Kings
+        if (card.value === ECardValue.KING) {
+            state.tableau
+                .filter(({cards}) => !cards.length)
+                .forEach(({id: pileId}) => targetPositions.push({position: EGameBoardPart.TABLEAU, pileId}))
+        } else {
+            const nextValueByPriority = nextByPriority(card.value);
+            const filterPredicate = (pile: IPile): boolean => {
+                const lastCardInPile = pile.cards[pile.cards.length - 1].card;
+                if (cardSuiteColor(lastCardInPile.suit) == cardSuiteColor(card.suit)) return false;
+                return nextValueByPriority == lastCardInPile.value;
+            }
+
+            state.tableau
+                .filter(({cards}) => cards.length)
+                .filter(filterPredicate)
+                .forEach(pile => targetPositions.push({position: EGameBoardPart.TABLEAU, pileId: pile.id}));
+        }
+    }
+
+
+    return targetPositions;
 }
